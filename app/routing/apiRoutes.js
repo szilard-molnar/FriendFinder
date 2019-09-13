f05151fa7c6f95bcfe96f9ea75b452f1a03536f8
szilard-molnar <mszilard86@gmail.com>
2019-09-12 22:11:07 -0400

    htmlRoutes work

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,34 @@
+var tableData = require("../data/tableData");
+var waitListData = require("../data/waitinglistData");
+
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
+    res.json(tableData);
+  });
+
+  app.get("/api/waitlist", function(req, res) {
+    res.json(waitListData);
+  });
+
+  app.post("/api/tables", function(req, res) {
+    
+    if (tableData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    }
+    else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
+
+
+  app.post("/api/clear", function(req, res) {
+    
+    tableData.length = 0;
+    waitListData.length = 0;
+
+    res.json({ ok: true });
+  });
+};
